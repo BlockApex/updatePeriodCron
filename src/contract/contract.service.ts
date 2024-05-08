c8f4e0dc663db73839b64854e461cb8b2ff82aaa
HareemSaad <hareems20014@outlook.com>
2024-05-08 22:23:28 +0500

    abi fix, import dotenv

--- src/contract/contract.service.ts
+++ src/contract/contract.service.ts
@@ -1,14 +1,16 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import * as ethers from 'ethers';
-import MINTER from '../abi/Minter.json';
+import {abi} from '../abi/Minter';
+import "dotenv/config";
 
 @Injectable()
 export class ContractService {
 
   private provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
   private signer = new ethers.Wallet(process.env.PRIVATE_KEY, this.provider);
-  private contractAddress =process.env.MINTER_CONTRACT;
-  private contractABI = MINTER;
+  private contractAddress = process.env.MINTER_CONTRACT;
+  private contractABI = abi;
 
   private contract = new ethers.Contract(
     this.contractAddress,
